d2db5c47f5268c2a0241864676ca8387ede42f26
daekunhan <dosemfb0920@gmail.com>
2021-06-01 14:49:24 +0900

    issue-tracker-init

---
+++ src/main/java/com/agile/issue/AgileIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.agile.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileIssueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileIssueTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agile/issue/AgileIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileIssueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
